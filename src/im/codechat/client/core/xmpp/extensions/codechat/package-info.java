f6f6fc3ea0caeb6a8f377ba6ac163843f21dc72a
Yemi Kudaisi <yemikudaisi@gmail.com>
2017-06-05 04:01:53 +0100

    Changed folder structure to maven style. Added Robot font.

--- src/im/codechat/client/core/xmpp/extensions/codechat/package-info.java
+++
@@ -1,6 +0,0 @@
-@XmlSchema(namespace = "http://xmpp.rocks", elementFormDefault = XmlNsForm.QUALIFIED)
-
-package im.codechat.client.core.xmpp.extensions.codechat;
-
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
